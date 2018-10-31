45bce69e9f0c9399a3de1da15da3e1676f4c495f
Vishesh <visheshsingh16@gmail.com>
2018-10-31 12:20:40 -0400

    Setting up a Pie Chart

---
+++ graph.js
@@ -0,0 +1,25 @@
+const dims = { height: 300, width: 300, radius: 150 };
+const center = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 150)
+  .attr("height", dims.height + 150);
+
+const graph = svg
+  .append("g")
+  .attr("transform", `translate(${center.x},${center.y})`);
+
+const pie = d3
+  .pie()
+  .sort(null)
+  .value(d => d.cost);
+
+const angles = pie([
+  { name: "rent", cost: 500 },
+  { name: "food", cost: 300 },
+  { name: "gaming", cost: 200 }
+]);
+
+console.log(angles);
