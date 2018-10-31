17ecced16285e7001af58a4c92234d429ceca0df
Vishesh <visheshsingh16@gmail.com>
2018-10-31 16:33:16 -0400

    Tooltips

--- graph.js
+++ graph.js
@@ -34,6 +34,18 @@ const legend = d3
   .shapePadding(10)
   .scale(color);
 
+// Tooltip
+const tip = d3
+  .tip()
+  .attr("class", "tip card")
+  .html(d => {
+    let content = `<div class="name">${d.data.name}</div>`;
+    content += `<div class="cost">$ ${d.data.cost}</div>`;
+    content += `<div class="delete">Click slice to delete</div>`;
+    return content;
+  });
+
+graph.call(tip);
 // update function
 const update = data => {
   // update color scale domain
@@ -78,8 +90,14 @@ const update = data => {
   // add events
   graph
     .selectAll("path")
-    .on("mouseover", handleMouseOver)
-    .on("mouseout", handleMouseOut)
+    .on("mouseover", (d, i, n) => {
+      tip.show(d, n[i]);
+      handleMouseOver(d, i, n);
+    })
+    .on("mouseout", (d, i, n) => {
+      tip.hide(d, n[i]);
+      handleMouseOut(d, i, n);
+    })
     .on("click", handleClick);
 };
 // data array
