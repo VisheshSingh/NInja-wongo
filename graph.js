788376cd02724e5b00775b79d24830adb030d3aa
Vishesh <visheshsingh16@gmail.com>
2018-10-31 12:49:33 -0400

    Real time data listener

--- graph.js
+++ graph.js
@@ -27,4 +27,33 @@ const arcPath = d3
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-console.log(arcPath(angles[0]));
+// update function
+const update = data => {
+  console.log(data);
+};
+// data array
+var data = [];
+
+// Realtime connection with firestore
+db.collection("expenses").onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter(item => item.id != doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+
+  update(data);
+});
